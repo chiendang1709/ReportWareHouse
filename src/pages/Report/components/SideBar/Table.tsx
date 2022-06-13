a0a2c5d7bd109f5cd0c894b6678146461b124ff5
RyuuKaiser <gauvu107@gmail.com>
2022-06-13 13:21:45 +0700

    sua cot bang

--- src/pages/Report/components/SideBar/Table.tsx
+++ src/pages/Report/components/SideBar/Table.tsx
@@ -5,35 +5,10 @@ import Field from './Field'
 
 const Table = ( props :{ listTable: any; name:string}) => {
   const [submenu, setSubmenu] = useState(false);
-  let ref = React.useRef<HTMLLIElement>(null)
-
-  useEffect(() => {
-    const handler = (event: TouchEvent | MouseEvent) => {
-      if (submenu && ref.current && !ref.current.contains(event.target as HTMLLIElement)){
-        setSubmenu(false);
-      }
-    };
-    document.addEventListener("mousedown", handler );
-    document.addEventListener("touchstart", handler );
-    return () => {
-      // Cleanup the event listener
-      document.removeEventListener("mousedown", handler );
-      document.removeEventListener("touchstart", handler );
-    };
-  }, [submenu]);
-  
-  const onMouseEnter = () => {
-    window.innerWidth > 200 && setSubmenu(true);
-  };
-  const onMouseLeave = () => {
-    window.innerWidth > 200  && setSubmenu(false);
-  };
- 
-  
 
   return (
       
-            <li ref={ref} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} >
+            <li  >
                 <button className='content__btn submenu__btn' 
                  type="button" aria-haspopup="menu" aria-expanded={submenu ? "true" : "false"}
                   onClick={()=> setSubmenu((prev) => !prev)}>
