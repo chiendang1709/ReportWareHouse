57e056899ba176d398fd40c1d8e8580acb63944b
Chiến <chiendang1011@gmail.com>
2022-06-29 09:09:29 +0700

    hello

--- src/pages/Report/components/Content/TableData.tsx
+++ src/pages/Report/components/Content/TableData.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { DataGrid, GridColDef,GridToolbarContainer,GridToolbar,GridCellParams} from '@mui/x-data-grid';
+import { DataGrid, GridColDef,GridToolbarContainer,GridToolbar,GridCellParams,GridToolbarExport,GridToolbarColumnsButton,GridToolbarFilterButton,GridToolbarDensitySelector} from '@mui/x-data-grid';
 
 import { useAppSelector } from 'app/store/hooks';
 import { styleMui } from 'components/common/styleMui';
@@ -36,6 +36,11 @@ const nameTV = [
     value_code: "Năm"
   },
   {
+   
+    key_code: "time",
+    value_code: "Thời Gian"
+  },
+  {
     key_code: "departments_name",
     value_code: "Bộ phận"
   },
@@ -51,16 +56,45 @@ const TableData = () => {
   var regex = new RegExp('^[0-9]*$')
   const columns: GridColDef[] = [];
   const rows:any =[];
+  const listChange:any =[]
   const classes = styleMui();
 
   const [on, setOn] = useState(false);
-
+  const [pageSize, setPageSize] = useState<number>(5);
  
   const onTable = useAppSelector(state=> state.onTable) 
   const listValueField = useAppSelector(state=> state.tableData) 
   const listTable = useAppSelector(state => state.table)
    
   useEffect(()=> setOn(onTable.onTable),[onTable])
+
+  if(listValueField.listData.length !== 0){
+    let nameField =Object.keys(listValueField.listData[0])
+    if(nameField.includes('month_name') && nameField.includes('year') )
+    {
+      listValueField.listData.map((valueField:any, index:number)=> {
+        let year =valueField["year"]
+        let month = valueField["month_name"]
+        let coppy ={...valueField}
+        coppy.time = `${month}/ ${year}`
+        delete coppy["year"]
+        delete coppy["month_name"]
+        listChange.push(coppy)      
+    })
+    } else  if(nameField.includes('month_name') ==false && nameField.includes('year') )
+    {
+      listValueField.listData.map((valueField:any, index:number)=> {
+        let year =valueField["year"]
+        let coppy ={...valueField}
+        coppy.time = `${year}`
+        delete coppy["year"]
+        listChange.push(coppy)      
+    })
+    }else if(nameField.includes('month_name') ==false && nameField.includes('year') ==false){
+      listValueField.listData.map((valueField:any, index:number)=> {
+        listChange.push(valueField)    
+    })
+  }}
 
 
   const formatter = new Intl.NumberFormat("it-IT", {
@@ -69,24 +103,36 @@ const TableData = () => {
     minimumFractionDigits: 3
   });
   const colums =()=>{
-    if(listValueField.listData.length !== 0){
-      let nameField =Object.keys(listValueField.listData[0])
+    if(listChange.length !== 0){
+      let nameField =Object.keys(listChange[0])
 
       nameField.map((nameField:string)=> {
-        for(let z = 0;z<nameTV.length; z++){
-          if(nameTV[z].key_code ==`${nameField}` && nameField !=="year" && nameField !=="month_name" && nameField !=="departments_name"  ){
-           columns.push({ field: nameField, headerName: nameTV[z].value_code, width: 180,renderCell: (params: GridCellParams) => formatter.format(params.value), type: "number"} )
-          } else if(nameTV[z].key_code ==`${nameField}` && nameField ==="year" || nameTV[z].key_code ==`${nameField}` && nameField ==="month_name" || nameTV[z].key_code ==`${nameField}` && nameField ==="departments_name") {
-           columns.push({ field: nameField, headerName: nameTV[z].value_code, width: 100,type: "number"} )  
-          }
-      }})
+        if(nameField =="departments_name"){
+          for(let z = 0;z<nameTV.length; z++){
+            if(nameTV[z].key_code ==`${nameField}`){
+              columns.push({ field: nameField, headerName: nameTV[z].value_code, width: 100,type: "number"} )  
+            }
+        }
+        }
+       })
+       nameField.map((nameField:string)=> {
+        if(nameField !=="departments_name"){
+          for(let z = 0;z<nameTV.length; z++){
+            if(nameTV[z].key_code ==`${nameField}` && nameField !=="time"){
+              columns.push({ field: nameField, headerName: nameTV[z].value_code, width: 180,renderCell: (params: GridCellParams) => formatter.format(params.value), type: "number"} )
+             } else if(nameTV[z].key_code ==`${nameField}` && nameField ==="time") {
+              columns.push({ field: nameField, headerName: nameTV[z].value_code, width: 100,type: "number"} )  
+             }
+        }
+        }
+       }) 
     }
    
   }
   
   const row = ()=>{
-    if(listValueField.listData.length !== 0){
-      listValueField.listData.map((valueField:any, index:number)=> {
+    if(listChange.length !== 0){
+      listChange.map((valueField:any, index:number)=> {
         const newObj ={...valueField}
         newObj.id= index
         rows.push(newObj)
@@ -100,12 +146,14 @@ const TableData = () => {
   function MyExportButton() {
     return (
       <GridToolbarContainer>
-        <GridToolbar
-        csvOptions={{
-          utf8WithBom: true,
-        }}
-        />
-      </GridToolbarContainer>
+      <GridToolbarExport
+      csvOptions={{
+        utf8WithBom: true,
+      }}
+      />
+    <GridToolbarColumnsButton />
+    <GridToolbarFilterButton />
+    </GridToolbarContainer>
     );
   }
   return (
@@ -118,6 +166,10 @@ const TableData = () => {
               rows={ rows}          
               columns={columns}
               components={{ Toolbar: MyExportButton }}  
+              pageSize={pageSize}
+              onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+              rowsPerPageOptions={[5, 10, 20]}
+              pagination
             />   
          )
         :<h1> choose your data</h1>
