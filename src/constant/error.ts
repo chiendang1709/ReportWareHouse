fc9c130ec431342d579cbbbfb414ea9e65b87add
Chiến <chiendang1011@gmail.com>
2022-07-16 18:36:58 +0700

    if else filter

--- src/constant/error.ts
+++ src/constant/error.ts
@@ -1,6 +1,8 @@
 export const error = {
     ERROR_NO_DATA : "No Data",
     ERROR_FAILD_API : "Error API",
-    ERROR_INPUT_YEAR : "PLEASE YEAR AGAIN",
+    ERROR_INPUT_YEAR : "Please Choose Year Again!",
+    ERROR_INPUT_MONTH: "Please Choose Month Again!",
+    ERROR_INPUT_DAY: "Please Choose Day Again!",
     ERROR_NO_FIELD : "No Data"
 }
--- src/pages/Report/components/Content/Tools.tsx
+++ src/pages/Report/components/Content/Tools.tsx
@@ -7,6 +7,7 @@ import { listDepartment } from 'interfaces/components';
 import { useAppDispatch, useAppSelector } from 'app/store/hooks';
 import { filterAction } from 'pages/Report/slice/filterSlice';
 import { ToastContainer, toast } from 'react-toastify';
+import { error } from 'constant/error';
 import undo  from 'assets/images/undo__icon.png'
 import fil from 'assets/images/filter__icon.png'
 import ex from 'assets/images/export__icon.png'
@@ -34,106 +35,106 @@ const Tools = () => {
   const [nameDep, setNameDep]=useState("");
   const [time, setTime] = useState("Day");
 
-  const [a, setA]= useState("")
-  const [b, setB]= useState("") 
+  const [dateFrom, setDateFrom]= useState("")
+  const [dateTo, setDateTo]= useState("") 
   const top100Films = ["Elephant","Zebra", "Tiger", "Rabbit"]
 
   //filter
-  useEffect(() => {
-    if(listFilter.listFilter.length >0){ 
-        // props.callBackData(listFilter.listFilter,'')  
-    } 
-  }, [listFilter.listFilter]);
-  const filter = ()=> {
-    let arrayCheck: string[]= []
-    let y1: string|boolean= year1
-    let m1: string|boolean= month1
-    let y2: string|boolean= year2
-    let m2: string|boolean= month2
-    let checkbox: any = document.getElementsByName('checkbox')  ;
-    for (let item of checkbox) {
-     if( item.checked == true){
-        arrayCheck.push(item.value)
-      }
-     }
-     // 4 null
-     if(year1 =="" && month1==="NULL" && year2 =="" && month2 ==="NULL" &&  dep==="NULL"){
-          let arrayCoppy =  listValueField.listValueField.slice()   
-          // props.callBackData(arrayCoppy,"") 
-     }   
-     else {
-        if(y1 =="" && m1==="NULL" && y2 =="" && m2 ==="NULL" &&  dep!=="NULL"){
-          m1 ="NULL"
-          y1 =""
-          m2 = "NULL"
-          y2 =""
-          setDep(dep)
-         }  else
-        if(y1 =="" && y2 ==""  ){
-          return toast.error("Please Choose Year!");
-         } 
-       else   
-        if(y1 !== "" && y2 !="" &&  Number(y1)  > Number(y2)){
-           return toast.error("Please Choose Year Again!");
-         }
-       else 
-         if(y1 && y2 && m2 == "NULL" && m1 =="NULL"){
-           y2 = y2
-         } 
-      else 
-         if(y1 === y2 && m1 !=="" && m2 !=="" && Number(m1)  > Number(m2) ){
+  // useEffect(() => {
+  //   if(listFilter.listFilter.length >0){ 
+  //       // props.callBackData(listFilter.listFilter,'')  
+  //   } 
+  // }, [listFilter.listFilter]);
+  // const filter = ()=> {
+  //   let arrayCheck: string[]= []
+  //   let y1: string|boolean= year1
+  //   let m1: string|boolean= month1
+  //   let y2: string|boolean= year2
+  //   let m2: string|boolean= month2
+  //   let checkbox: any = document.getElementsByName('checkbox')  ;
+  //   for (let item of checkbox) {
+  //    if( item.checked == true){
+  //       arrayCheck.push(item.value)
+  //     }
+  //    }
+  //    // 4 null
+  //    if(year1 =="" && month1==="NULL" && year2 =="" && month2 ==="NULL" &&  dep==="NULL"){
+  //         let arrayCoppy =  listValueField.listValueField.slice()   
+  //         // props.callBackData(arrayCoppy,"") 
+  //    }   
+  //    else {
+  //       if(y1 =="" && m1==="NULL" && y2 =="" && m2 ==="NULL" &&  dep!=="NULL"){
+  //         m1 ="NULL"
+  //         y1 =""
+  //         m2 = "NULL"
+  //         y2 =""
+  //         setDep(dep)
+  //        }  else
+  //       if(y1 =="" && y2 ==""  ){
+  //         return toast.error("Please Choose Year!");
+  //        } 
+  //      else   
+  //       if(y1 !== "" && y2 !="" &&  Number(y1)  > Number(y2)){
+  //          return toast.error("Please Choose Year Again!");
+  //        }
+  //      else 
+  //        if(y1 && y2 && m2 == "NULL" && m1 =="NULL"){
+  //          y2 = y2
+  //        } 
+  //     else 
+  //        if(y1 === y2 && m1 !=="" && m2 !=="" && Number(m1)  > Number(m2) ){
          
-          let object ={
-            params: arrayCheck.join(","),
-            year:y1,
-            month: m1,
-            year2:  y2,
-            month2: m2  ,
-            departments: dep
-          } 
+  //         let object ={
+  //           params: arrayCheck.join(","),
+  //           year:y1,
+  //           month: m1,
+  //           year2:  y2,
+  //           month2: m2  ,
+  //           departments: dep
+  //         } 
          
           
-          return  toast.error("Please Choose Month Again!");
-         } 
-        else
-          if(y1 !=="" && y2 !=="" && m2 !== "NULL" && m1 !=="NULL"){
-            y1 =y1
-            y2 =y2
-            m1 =m1
-            m2 = m2
-          }
-        else 
-         if(y1 ==false || m1 == false && y2 && m2 )
-         {
-           y1 =y2
-           m1 = m2
-           y2 = false
-           m2 = false
-         } 
-        else 
-         if(m1 =="NULL" && y1 !=="" && m2 && y2 ==""){
-           m1 ="NULL"
-           y1 =y1
-           m2 = "NULL"
-           y2 =""
-        }  
-         else
-          if(m1 !== "NULL" && y1 !=="" && y2 !=="" || m1 !=="NULL"){
-            m1 =m1
-            y1 = y1
-            y2 =""
-            m2 = "NULL"
-          }
-        let object ={
-          params: arrayCheck.join(","),
-          year:y1? y1 : "NULL",
-          month: m1 ? m1: "NULL",
-          year2:  y2? y2 : "NULL",
-          month2: m2  ? m2 : "NULL",
-          departments:  Number(dep) ? dep: "NULL"
-        } 
-         dispatch(filterAction.getFilter(object))
-     } 
+  //         return  toast.error("Please Choose Month Again!");
+  //        } 
+  //       else
+  //         if(y1 !=="" && y2 !=="" && m2 !== "NULL" && m1 !=="NULL"){
+  //           y1 =y1
+  //           y2 =y2
+  //           m1 =m1
+  //           m2 = m2
+  //         }
+  //       else 
+  //        if(y1 ==false || m1 == false && y2 && m2 )
+  //        {
+  //          y1 =y2
+  //          m1 = m2
+  //          y2 = false
+  //          m2 = false
+  //        } 
+  //       else 
+  //        if(m1 =="NULL" && y1 !=="" && m2 && y2 ==""){
+  //          m1 ="NULL"
+  //          y1 =y1
+  //          m2 = "NULL"
+  //          y2 =""
+  //       }  
+  //        else
+  //         if(m1 !== "NULL" && y1 !=="" && y2 !=="" || m1 !=="NULL"){
+  //           m1 =m1
+  //           y1 = y1
+  //           y2 =""
+  //           m2 = "NULL"
+  //         }
+  //       let object ={
+  //         params: arrayCheck.join(","),
+  //         year:y1? y1 : "NULL",
+  //         month: m1 ? m1: "NULL",
+  //         year2:  y2? y2 : "NULL",
+  //         month2: m2  ? m2 : "NULL",
+  //         departments:  Number(dep) ? dep: "NULL"
+  //       } 
+  //        dispatch(filterAction.getFilter(object))
+  //    } 
      
     
     // if(nameDep === "Choose Despartment")
@@ -143,7 +144,7 @@ const Tools = () => {
     //     props.callBackData(listFilter.listFilter,nameDep)   
     // }
    
-  }
+//}
 
 //undo
     const undos = ()=>{
@@ -152,26 +153,59 @@ const Tools = () => {
        }
 //filter test
 const filtertest= ()=>{
+  let date1 = dateFrom.split('-')
+  let date2 =dateTo.split('-')
   console.log("dep",depart);
   console.log("staff",staff);
   console.log("cus",customer);
   console.log("mvv",mvv);
-  console.log("date", `from ${a} to ${b} `);
-
+ 
+  switch(time){
+    case 'Day': 
+       if(date1[0] > date2[0]){
+         return toast.error(`${error.ERROR_INPUT_YEAR}`);
+       } 
+       else if(date1[0]===date2[0] && date1[1]> date2[1]) {
+         return toast.error(`${error.ERROR_INPUT_MONTH}`);
+       }
+       else if (date1[0]=== date2[0] && date1[1]===date2[1] && date1[2]> date2[2]) {
+         return toast.error(`${error.ERROR_INPUT_DAY}`);
+       }
+       console.log("day", `from ${date1} to ${date2} `);     
+       break;
+    case 'Month': 
+       if(date1[0] > date2[0]){
+         return toast.error(`${error.ERROR_INPUT_YEAR}`);
+       } 
+       else if(date1[0]===date2[0] && date1[1]> date2[1]) {
+         return toast.error(`${error.ERROR_INPUT_MONTH}`);
+       }
+       console.log("motnh", `from ${date1} to ${date2} `);     
+       break;
+    case 'Year':
+      if(date1[0] > date2[0]){
+        return toast.error(`${error.ERROR_INPUT_YEAR}`);
+      } 
+      console.log("year", `from ${date1} to ${date2} `);
+      break;
+    default: 
+      return null;
+  }
   
 }
 const renderContent = React.useCallback(() => {
   switch(time) {
    
     case 'Day': 
+      
       return <div className='time__date'>
                  <div className="time__group">
                       <label htmlFor="day" className="time__label"> From (Day) </label>
-                      <input type="date" className="time__field" placeholder="Day" name="day" id='day' onChange={(e)=> setA(e.target.value)} required />
+                      <input type="date" className="time__field" placeholder="Day" name="day" id='day' onChange={(e)=> setDateFrom(e.target.value)} required />
                  </div>
                  <div className="time__group">
                       <label htmlFor="day" className="time__label"> To (Day) </label>
-                      <input type="date" className="time__field" placeholder="Day" name="day" id='day'  onChange={(e)=> setB(e.target.value)} required />
+                      <input type="date" className="time__field" placeholder="Day" name="day" id='day'  onChange={(e)=> setDateTo(e.target.value)} required />
                  </div>
             </div>;
     
@@ -180,11 +214,11 @@ const renderContent = React.useCallback(() => {
       return  <div className='time__date'>
               <div className="time__group">
                    <label htmlFor="Month" className="time__label"> From (Month) </label>
-                   <input type="month" className="time__field" placeholder="Month" name="Month" id='Month' onChange={(e)=> setA(e.target.value)} required />
+                   <input type="month" className="time__field" placeholder="Month" name="Month" id='Month' onChange={(e)=> setDateFrom(e.target.value)} required />
               </div>
               <div className="time__group">
                    <label htmlFor="Month" className="time__label"> To (Month) </label>
-                   <input type="month" className="time__field" placeholder="Month" name="Month" id='Month' onChange={(e)=> setB(e.target.value)} required />
+                   <input type="month" className="time__field" placeholder="Month" name="Month" id='Month' onChange={(e)=> setDateTo(e.target.value)} required />
               </div>
             </div>;
       
