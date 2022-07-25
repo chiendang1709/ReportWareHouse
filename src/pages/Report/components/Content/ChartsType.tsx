deb02d513701f8f4f7d2b53aab82baaf1b9b7478
Ryuukaiser <gauvu107@gmail.com>
2022-07-25 21:12:13 +0700

    sua chữ

--- src/pages/Report/components/Content/ChartsType.tsx
+++ src/pages/Report/components/Content/ChartsType.tsx
@@ -20,7 +20,7 @@ import pie from 'assets/images/pie__icon.png'
      return (
         <div className='group__chart'>
             <div className="group__title">
-                LOẠI CHART
+                LOẠI BIỂU ĐỒ
             </div>
             <div className="group__item">
              <button className={`chart__button ${ typeCharts.typeChart=='bar' ? 'active__chart' : ''}`} 
